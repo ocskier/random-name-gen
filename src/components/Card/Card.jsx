96af41f82b1571acdfb1f6de1f6ca7074ba90dbf
Jon Jackson <ocskier@gmail.com>
2021-07-29 16:30:05 -0400

    feat: adds peeps

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -107,7 +107,7 @@ export const LearnerCard = ({ soundEl }) => {
     window.myInterval = window.setInterval(() => {
       setRandomColor(colors[Math.floor(Math.random() * colors.length)]);
       setRandomPerson(persons[Math.floor(Math.random() * persons.length)]);
-    }, 300);
+    }, 200);
   };
 
   return (
