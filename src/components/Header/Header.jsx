0552475d30b11a881aaabce1af83dcefd5a49487
Jon Jackson <ocskier@gmail.com>
2021-07-30 10:21:29 -0400

    style: adds gradient to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,13 @@ import { Advertisement } from 'semantic-ui-react';
 const styles = {
   header: {
     width: '100%',
-    backgroundColor: 'mediumblue',
+    background: 'rgb(9, 20, 164)',
+    background:
+      '-moz-radial-gradient(center, rgb(9,20,164) 0%, rgb(35,136,203) 60%, rgb(10,77,117) 100%)',
+    background:
+      '-webkit-radial-gradient(center, rgb(9,20,164) 0%, rgb(35,136,203) 60%, rgb(10,77,117) 100%)',
+    background:
+      'radial-gradient(ellipse at center, rgb(9,20,164) 0%, rgb(35,136,203) 60%, rgb(10,77,117) 100%)',
   },
 };
 
